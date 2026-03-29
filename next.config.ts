fb5928a8bcca7723f9a922538e5ab4026de2034f
Ashutosh kumar <ashutosh.kumar5876@gmail.com>
2026-03-29 08:57:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'static.tvmaze.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
